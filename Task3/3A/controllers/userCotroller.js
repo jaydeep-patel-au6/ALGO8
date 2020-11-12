2c896761e2685071095ee1b715ac688080c252ee
Jaydeep Patel <58411819+jaydeep-patel-au6@users.noreply.github.com>
2020-11-12 23:20:31 +0530

    user registration

--- Task3/3A/controllers/userCotroller.js
+++ Task3/3A/controllers/userCotroller.js
@@ -1,8 +1,34 @@
+const User = require('../models/userModel')
+const {auth} = require('../middlewares/auth')
+
+
+
 class user {
 
     homePage(req, res){
         res.status(200).send('login sucessfull');
     }
+
+    signUp(req, res){
+    
+        const newuser=new User(req.body);
+        
+        if(newuser.password!=newuser.password2)return res.status(400).json({message: "password does not match"});
+            
+            User.findOne({email:newuser.email},function(err,user){
+
+                if(user) return res.status(400).json({ auth : false, message :"alrady register with this email"});
+        
+                newuser.save((err,doc)=>{
+                    if(err) {console.log(err);
+                        return res.status(400).json({ success : false});}
+                    res.status(200).json({
+                        succes:true,
+                        user : doc
+                    });
+                });
+            });
+            }
 
 }
 
--- Task3/3A/routes/userRoutes.js
+++ Task3/3A/routes/userRoutes.js
@@ -3,8 +3,11 @@ const express = require('express')
 const router = express.Router()
 const User = require('../controllers/userCotroller')
 
+
 const user = new User()
 
 router.get('/', user.homePage) //home page route
+
+router.post('/api/register', user.signUp) //register user route
 
 module.exports = router
